fa09e1ecb29983bc2c48878c43c1118d939b72cd
Thore Goll <thoregoll@googlemail.com>
2023-05-05 11:05:59 +0200

    CHANGE: First prototype with custom electronic shield for ESP8266

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # sIoThy
 
-Soild IoT Humidity Monitor
+Soil IoT Humidity Monitor
+
+## ESP8266
+
+![pinout](https://i0.wp.com/edistechlab.com/wp-content/uploads/2021/04/WeMos-d1-mini-Pin-out.png)
+
+
+
+## ADC-Mux Shield
+
+Since the ESP8266 has only one analog input (`A0`), we have to use a analog signal multiplexer. For that we
+use the [74HC4051D](https://www.mouser.com/datasheet/2/408/74HC4051D_datasheet_en_20201203-959201.pdf):
+
+![74HC4051D](./74hc4051D.png)
+
+This is mounted on a customly designed shield, where the ESP socks into:
+
+![shield](./shield.jpg)
--- slothy.ino
+++ slothy.ino
@@ -1,9 +1,25 @@
-#define D1   5
-#define D2   4
-#define D3   0
+#define D0       16  
+#define D1        5
+#define D2        4
+#define D3        0
+#define D4        2
+#define D5       14
+#define D6       12
+#define D7       13
+#define D8       15 
 
-#define ANA_SRC_VOLTAGE    7
-#define ANA_SRC_HUMID0     2
+#define MUX_A   D6
+#define MUX_B   D5
+#define MUX_C   D0
+
+#define ANA_SRC_VOLTAGE    6
+#define ANA_SRC_HUMID_0    5
+#define ANA_SRC_HUMID_1    3
+#define ANA_SRC_HUMID_2    1
+#define ANA_SRC_HUMID_3    7
+#define ANA_SRC_HUMID_4    2
+#define ANA_SRC_HUMID_5    0
+
 #define ANA_VOLTAGE_REF    3.3     // Reference voltage corresponding to 1023 of ADC [V]
 
 #define HUMIDITY_ADC_DRY   800     // ADC value when reading complete dry soil [0..1023]
@@ -11,24 +27,38 @@
 #define VOLTAGE_DIVIDER_R1 92e3    // Low-side resistor of voltage divider [Ohm]
 #define VOLTAGE_DIVIDER_R2 48e3    // High-side resistor of voltage divider [Ohm]
 
+char ANA_SRC_HUMIDS[6] = {
+  ANA_SRC_HUMID_0,
+  ANA_SRC_HUMID_1,
+  ANA_SRC_HUMID_2,
+  ANA_SRC_HUMID_3,
+  ANA_SRC_HUMID_4,
+  ANA_SRC_HUMID_5
+};
+
 void setup() {
   Serial.begin(9600);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
+  pinMode(MUX_A, OUTPUT);
+  pinMode(MUX_B, OUTPUT);
+  pinMode(MUX_C, OUTPUT);
   pinMode(A0, INPUT);
 }
 
 
 int readAnalogFrom(char channel) {
-  digitalWrite(D1, channel & (1 << 0));
-  digitalWrite(D2, channel & (1 << 1));
-  digitalWrite(D3, channel & (1 << 2));
+  digitalWrite(MUX_A, channel & (1 << 0));
+  digitalWrite(MUX_B, channel & (1 << 1));
+  digitalWrite(MUX_C, channel & (1 << 2));
   return analogRead(A0);
 }
 
 float readHumitiy(char channel) {
-  return ((float)readAnalogFrom(channel) - HUMIDITY_ADC_WET) / (HUMIDITY_ADC_DRY - HUMIDITY_ADC_WET);
+  int x = readAnalogFrom(channel);
+  if(x > HUMIDITY_ADC_DRY + 25 || x < HUMIDITY_ADC_WET - 25) {
+    return NAN;
+  }
+  
+  return 100.0 - 100.0 * ((float)x - HUMIDITY_ADC_WET) / (HUMIDITY_ADC_DRY - HUMIDITY_ADC_WET);
 }
 float readVoltage() {
   float x = readAnalogFrom(ANA_SRC_VOLTAGE);
@@ -36,8 +66,12 @@ float readVoltage() {
 }
 
 void loop() {
-  Serial.print(">> Humidity: "); Serial.print(readHumitiy(ANA_SRC_VOLTAGE)); Serial.println(" %");
-  delay(2500);
   Serial.print(">> Voltage: "); Serial.print(readVoltage()); Serial.println(" V");
+  for (int i = 0; i < 6; i++) {
+    Serial.print(">> Humidity #: "); Serial.print(i); Serial.print(": "); 
+    Serial.print(readHumitiy(ANA_SRC_HUMIDS[i]), 0); 
+    Serial.println(" %");
+  }
+  
   delay(2500);
 }
