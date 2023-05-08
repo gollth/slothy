7af20d19f465c24e637120667174f2930814ec87
Thore Goll <thoregoll@goolgemail.com>
2023-05-08 10:28:43 +0200

    WIP: `GET /water/{plant}` route

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ mod router;
 
 use crate::router::homepage;
 use actix_web::web::ServiceConfig;
+use router::get_water;
 
 pub fn server(config: &mut ServiceConfig) {
-    config.service(homepage);
+    config.service(homepage).service(get_water);
 }
 
 #[cfg(test)]
@@ -22,6 +23,15 @@ mod tests {
         let app = init_service(App::new().configure(server)).await;
         let request = TestRequest::default().to_request();
 
+        let response = call_service(&app, request).await;
+        assert_eq!(StatusCode::OK, response.status());
+        assert_eq!(Bytes::from_static(b"Slothy"), read_body(response).await);
+    }
+
+    #[actix_web::test]
+    async fn test_get_water_for_mint_plant() {
+        let app = init_service(App::new().configure(server)).await;
+        let request = TestRequest::default().uri("/water/mint").to_request();
         let response = call_service(&app, request).await;
         assert_eq!(StatusCode::OK, response.status());
         assert_eq!(Bytes::from_static(b"Slothy"), read_body(response).await);
--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,13 @@
-use actix_web::{get, HttpRequest, HttpResponse, Responder};
+use actix_web::{get, web::Path, HttpRequest, HttpResponse, Responder, Result};
 
 #[get("/")]
 async fn homepage(_request: HttpRequest) -> impl Responder {
     HttpResponse::Ok().body("Slothy")
+}
+
+#[get("/water/{plant}")]
+async fn get_water(path: Path<String>) -> Result<String> {
+    let plant = path.into_inner();
+    let level = 42.0;
+    Ok(format!("Current water level of {plant} is {level}"))
 }
